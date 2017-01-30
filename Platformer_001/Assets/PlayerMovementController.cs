9f784dbfe6bd207fd7cdde7ca607c0fb175a7006
Pablo <platz06@rg.lokal>
2017-01-30 11:06:27 +0100

    Ergebnis Erster Tag
    Gemeinsames Ergebnis

---
+++ Platformer_001/Assets/PlayerMovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) {
+			rb.velocity = new Vector2 (speed, 0f);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			rb.velocity = new Vector2 (-speed, 0f);
+		}
+	}
+}
