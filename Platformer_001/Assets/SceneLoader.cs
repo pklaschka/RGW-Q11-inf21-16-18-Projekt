4223c3f20015682216659aa1fc4c9f433960fff0
pklaschka <pabloklaschka@yahoo.de>
2017-02-13 11:43:20 +0100

    Ladebildschirm für Level hinzugefügt

---
+++ Platformer_001/Assets/SceneLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+	public string NameSceneToLoad;
+
+	public Image BackgroundImage;
+	public Slider ProgressBar;
+
+	private bool sceneLoading;
+	AsyncOperation async;
+
+	public void LoadScene() {
+		if (NameSceneToLoad != null && BackgroundImage != null && ProgressBar != null) {
+			BackgroundImage.GetComponent<Image>().enabled = true;
+
+			Scene sceneToLoad = SceneManager.GetSceneByName(NameSceneToLoad);
+
+			if (sceneToLoad != null) {
+				sceneLoading = true;
+				StartCoroutine(StartSceneLoad());
+			}
+		} else {
+			Debug.LogError("Szene konnte nicht geladen werden, da ein nötiger Paramter nicht festgelegt wurde.");
+		}
+	}
+
+	IEnumerator StartSceneLoad() {
+		async = SceneManager.LoadSceneAsync(NameSceneToLoad);
+		if( null != async )
+			async.allowSceneActivation = false;
+		yield return async;
+	}
+
+	void Update() {
+		if (null == async)
+			return;
+
+		ProgressBar.value = async.progress/0.9f;
+		if (async.progress >= 0.9f)
+			async.allowSceneActivation = true;
+	}
+}
