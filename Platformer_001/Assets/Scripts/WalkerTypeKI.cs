f2c6e3d94888fe9c44b07171a8b61869db4e55ab
Mememememememe <platz03@rg.lokal>
2017-02-20 11:04:45 +0100

    WalkerTypeKI hinzugefügt(Noch Buggy)

---
+++ Platformer_001/Assets/Scripts/WalkerTypeKI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkerTypeKI : MonoBehaviour {
+	public GameObject walkerObject;
+	public float speed;
+
+	// Debug:
+	public float rayLength = 2f;
+
+	private Rigidbody2D walker;
+	private BoxCollider2D walkerCollider;
+	// Use this for initialization
+	void Start () {
+		walker = GetComponent<Rigidbody2D>();
+		walkerCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		WalkRight ();
+		bool wallCheckRight = Physics2D.Raycast (new Vector2 (walkerObject.transform.position.x, walkerObject.transform.position.y), new Vector2 ((walkerObject.transform.position.x + 1), walkerObject.transform.position.y), rayLength);
+		bool wallCheckLeft = Physics2D.Raycast (new Vector2 (walkerObject.transform.position.x, walkerObject.transform.position.y), new Vector2 ((walkerObject.transform.position.x - 1), walkerObject.transform.position.y), rayLength);
+		if (wallCheckLeft) {
+			WalkRight ();
+			print("NachRechts");
+		}
+		if (wallCheckRight) {
+			WalkLeft ();
+			print ("NachLinks");
+		}
+	}
+
+	public void WalkLeft(){
+	walker.velocity = new Vector2 (-speed, walker.velocity.y);
+	}
+
+	public void WalkRight(){
+	walker.velocity = new Vector2 (speed, walker.velocity.y);
+	}
+}
+
