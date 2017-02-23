5f488729257c4ef9cfe99178ca0189724036132d
S-Florian <florian.strunz@gmx.de>
2017-02-24 00:36:09 +0100

    Feuerball-Wand Kollisionspartikel

--- Platformer_001/Assets/Scripts/Feuerball.cs
+++ Platformer_001/Assets/Scripts/Feuerball.cs
@@ -7,6 +7,8 @@ public class Feuerball : MonoBehaviour {
     public float lebensdauer = 2.0f;
     public Richtung bewegungsrichtung = Richtung.RECHTS;
 
+    public GameObject wandpartikel;
+
     private float lebenszeit = 0.0f;
     
 	void Update() {
@@ -22,6 +24,17 @@ public class Feuerball : MonoBehaviour {
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
+        if (wandpartikel != null && collision.contacts.Length > 0 && collision.gameObject.isStatic) {
+            Vector2[] vs = new Vector2[collision.contacts.Length];
+
+            for (int i = 0; i < vs.Length; ++i) {
+                vs[i] = collision.contacts[i].point;
+            }
+
+            var m = Helfer.MittelpunktFinden(vs);
+            var rot = Quaternion.AngleAxis((float)bewegungsrichtung * -90.0f, Vector3.forward);
+            var wp = Instantiate(wandpartikel, new Vector3(m.x, m.y, 0.0f), rot);
+        }
         // TODO: Betroffenem GameObject Schaden zufügen, wenn möglich
         Destroy(gameObject);
     }
--- Platformer_001/Assets/Scripts/Helfer.cs
+++ Platformer_001/Assets/Scripts/Helfer.cs
@@ -7,4 +7,20 @@ public static class Helfer {
         yield return new WaitForSeconds(sekunden);
         f();
     }
+
+    public static Vector2 MittelpunktFinden(params Vector2[] punkte) {
+        if (punkte.Length <= 0) return Vector2.zero;
+
+        float tx = 0, ty = 0;
+
+        foreach (var p in punkte) {
+            tx += p.x;
+            ty += p.y;
+        }
+
+        return new Vector2(
+            tx / punkte.Length,
+            ty / punkte.Length
+        );
+    }
 }
