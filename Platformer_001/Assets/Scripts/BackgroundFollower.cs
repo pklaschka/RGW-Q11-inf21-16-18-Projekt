529d2ecddb306cb4e4fb7f1dceed3aa55eabbece
M-Lampert <moritzlampert@yahoo.de>
2017-03-24 18:22:57 +0100

    Backgroundfollower finished
    Problem: zu klein um gesamten Hintergrund abzudecken

--- Platformer_001/Assets/Scripts/BackgroundFollower.cs
+++ Platformer_001/Assets/Scripts/BackgroundFollower.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class BackgroundFollower : MonoBehaviour {
 
 	public GameObject Player;
-	float pos;
-	// Use this for initialization
+	public GameObject Background;
+	public Vector3 offset;
+	float pos = 0;
+	GameObject bg1;
+	GameObject bg2;
+	GameObject bg3;
+	 //Use this for initialization
 	void Start () {
-		pos = 0;
+		bg1 = Instantiate(Background);
+		bg2 = Instantiate (Background);
+		bg3 = Instantiate (Background);
+		bg1.transform.position = new Vector3 (0, 0, 0) + offset;
+		bg2.transform.position = new Vector3 (0, 0, 0) + offset;
+		bg3.transform.position = new Vector3 (60, 0, 0) + offset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Player.transform.position.x >= pos + 15f){
-			this.gameObject.transform.position += new Vector3(0,15);
-			pos += 15;
+		if(Player.transform.position.x >= pos + 30f){
+			bg2.transform.position = new Vector3(pos + 60, 0) + offset;
+			bg3.transform.position = new Vector3 (pos + 120, 0) + offset;
+			var bg4 = bg1;
+			bg1 = bg2;
+			bg2 = bg4;
+			pos += 60;
+			print ("done" + pos);
 		}
 	}
 }
