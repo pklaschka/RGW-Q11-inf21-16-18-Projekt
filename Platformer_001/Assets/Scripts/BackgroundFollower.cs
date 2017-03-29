9ba597299bbbeca7740b736c2b2e02db44c7e0c1
M-Lampert <platz02@rg.lokal>
2017-03-29 11:50:20 +0200

    Background size change

--- Platformer_001/Assets/Scripts/BackgroundFollower.cs
+++ Platformer_001/Assets/Scripts/BackgroundFollower.cs
@@ -18,18 +18,18 @@ public class BackgroundFollower : MonoBehaviour {
 		bg3 = Instantiate (Background);
 		bg1.transform.position = new Vector3 (0, 0, 0) + offset;
 		bg2.transform.position = new Vector3 (0, 0, 0) + offset;
-		bg3.transform.position = new Vector3 (60, 0, 0) + offset;
+		bg3.transform.position = new Vector3 (120, 0, 0) + offset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Player.transform.position.x >= pos + 30f){
-			bg2.transform.position = new Vector3(pos + 60, 0) + offset;
-			bg3.transform.position = new Vector3 (pos + 120, 0) + offset;
+		if(Player.transform.position.x >= pos + 60f){
+			bg2.transform.position = new Vector3(pos + 120, 0) + offset;
+			bg3.transform.position = new Vector3 (pos + 240, 0) + offset;
 			var bg4 = bg1;
 			bg1 = bg2;
 			bg2 = bg4;
-			pos += 60;
+			pos += 120;
 			print ("done" + pos);
 		}
 	}
