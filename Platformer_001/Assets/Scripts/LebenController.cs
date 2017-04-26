8a0e4a2e797a1f94051cc27dc4f30b73938044b3
S-Florian <str1florian@gmail.com>
2017-04-26 14:17:32 +0430

    Schriftart gefixt und LebenController

--- Platformer_001/Assets/Scripts/LebenController.cs
+++ Platformer_001/Assets/Scripts/LebenController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class LebenController : MonoBehaviour {
 	public int leben;
-	public string gameOverName = Game_Over_Screen;
+	public string gameOverName = "Game_Over_Screen";
 
 	// Use this for initialization
 	void Start () {
