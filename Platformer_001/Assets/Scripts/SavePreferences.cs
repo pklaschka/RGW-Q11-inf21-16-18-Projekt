e15d6752c6ef231a2ca8664c83c9d8f6e9c93649
p1schneider1 <123>
2017-05-08 11:02:59 +0200

    Player Preferences

---
+++ Platformer_001/Assets/Scripts/SavePreferences.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePreferences : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*PlayerPrefs.SetFloat ("loudness", 0.5f);
+		PlayerPrefs.Save ();
+
+		PlayerPrefs.GetFloat ("loudness");
+	}
+}
