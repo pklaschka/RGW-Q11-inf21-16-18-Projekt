0eb7aa841e61b585df2563009226f57e59f5a4e5
pklaschka <pabloklaschka@yahoo.de>
2017-05-08 23:35:10 +0200

    Abänderung des Musiksystems für einfachere Musik-Generierung

---
+++ Platformer_001/Assets/Scripts/Procedural Music/MusicController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+	private GameObject[] tones;
+
+	public int beatsPerMinute = 120;
+	public int quartersPerBar = 4;
+	private float ticks = 0f;
+
+	// Computational Attributes
+	private int lastBar = 0;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (transform.gameObject);
+		tones = new GameObject[] {
+			transform.Find("l1").gameObject,
+			transform.Find("l2").gameObject,
+			transform.Find("l3").gameObject,
+			transform.Find("l4").gameObject,
+			transform.Find("l5").gameObject,
+			transform.Find("l6").gameObject,
+			transform.Find("l7").gameObject,
+			transform.Find("h1").gameObject,
+			transform.Find("h2").gameObject,
+			transform.Find("h3").gameObject,
+			transform.Find("h4").gameObject,
+			transform.Find("h5").gameObject,
+			transform.Find("h6").gameObject,
+			transform.Find("h7").gameObject
+		};
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		ticks += Time.deltaTime;
+
+		var currentBar = Mathf.FloorToInt(ticks * beatsPerMinute / (quartersPerBar * 60f));
+
+		if (currentBar != lastBar) {
+			lastBar = currentBar;
+
+			print("Bar: " + currentBar);
+			playChord (currentBar);
+		}
+	}
+
+	void playChord(int number) {
+		while (number > 7) {
+			number -= 7;
+		}
+		deactivateAll();
+
+		tones [number - 1].SetActive (true);
+		tones [number - 1 + 2].SetActive (true);
+		tones [number - 1 + 4].SetActive (true);
+	}
+
+	void deactivateAll() {
+		foreach (var tone in tones) {
+			tone.SetActive (false);
+		}
+	}
+}
---
+++ Platformer_001/Assets/Scripts/Procedural Music/Tone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class Tone : MonoBehaviour {
+	public double frequency = 440;
+
+	public enum audioMethod{
+		Sinus,
+		Sqrt
+	}
+
+	[Range(0.01f,5f)]
+	public double gain = 0.05f;
+
+	private double increment;
+	private double phase;
+	private double sampling_frequency = 48000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnAudioFilterRead(float[] data, int channels)
+	{
+		increment = frequency * 2 * Mathf.PI / sampling_frequency;
+		for (var i = 0; i < data.Length; i += channels) {
+			phase += increment;
+			// this is where we copy audio data to make them “available” to Unity
+			data [i] = (float)(gain * Math.Sin (phase));
+
+			// if we have stereo, we copy the mono data to each channel
+			if (channels == 2)
+				data [i + 1] = data [i];
+			
+			if (phase > 2 * Math.PI)
+				phase = 0;
+		
+		}
+	}
+}
+	
