35afb94d00c11c0b07fcb427df9fdb0c46294f4a
M-Lampert <moritzlampert@yahoo.de>
2017-05-10 12:02:41 +0200

    Highscore eingefügt

---
+++ Platformer_001/Assets/Scripts/Highscore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highscore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start (){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void saveLength(int length) {
+		if (length > PlayerPrefs.GetInt("maxEndlosweite")) {
+			PlayerPrefs.SetInt ("maxEndlosweite", length);
+		}
+		PlayerPrefs.SetInt ("Endlosweite", length);
+		print ("Score:" + PlayerPrefs.GetInt ("Endlosweite"));
+		print ("Highscore:" + PlayerPrefs.GetInt ("maxEndlosweite"));
+	}
+}
