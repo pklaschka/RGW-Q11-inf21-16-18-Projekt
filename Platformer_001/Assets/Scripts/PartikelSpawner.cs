4bd34f6095f5a82979890ca25fa0e7907f35bdf1
S-Florian <str1florian@gmail.com>
2017-05-24 12:04:37 +0200

    Partikel werden gespawnt

---
+++ Platformer_001/Assets/Scripts/PartikelSpawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartikelSpawner : MonoBehaviour {
+	public GameObject systemPrefab;
+
+	public void Spawnen(Transform ort) {
+		Instantiate(systemPrefab, new Vector3(ort.position.x, ort.position.y, ort.position.z), Quaternion.identity);
+	}
+}
