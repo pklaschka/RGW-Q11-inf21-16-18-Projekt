009bfbf62d214d6c4f81cb2b92b958ac8371b712
pklaschka <pabloklaschka@yahoo.de>
2017-05-25 00:57:38 +0200

    Levelauswahl revolutioniert (komplett automatisiert)
    fixes #74

---
+++ Platformer_001/Assets/Scripts/KampagneSceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KampagneSceneLoader : SceneLoader {
+	public int sceneIndex = 0;
+
+	public void LoadScene() {
+		PlayerPrefs.SetInt ("level_current", sceneIndex);
+		PlayerPrefs.Save ();
+		base.LoadScene ();
+	}
+}
