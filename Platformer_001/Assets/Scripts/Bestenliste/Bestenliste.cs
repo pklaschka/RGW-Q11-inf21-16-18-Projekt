355e995be312048884eaaf6fba521fddf905bea9
pklaschka <pabloklaschka@yahoo.de>
2017-05-28 19:41:09 +0200

    Erste Verbindungen zum Server!

--- Platformer_001/Assets/Scripts/Bestenliste/Bestenliste.cs
+++ Platformer_001/Assets/Scripts/Bestenliste/Bestenliste.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Collections;
 using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class Bestenliste : MonoBehaviour
 {
-    private static string serverAdress = "pi";
+    public static string serverAdress = "http://bestenliste:meineGemeineBestenliste@pi/inf21platformer";
 
-    public VerticalLayoutGroup scoreList;
+    private static string best5Adress = "getBestFive.php";
+    private static string rankAdress = "getRank.php?score=";
+    private static string submitAdress = "submitScore.php";
+
+    public Text bestenliste;
     public Text ownScore;
     public InputField nameInput;
 
@@ -18,9 +24,10 @@ public class Bestenliste : MonoBehaviour
     {
         score = PlayerPrefs.GetInt("maxEndlosweite", 0);
         scoreConfirmation = PlayerPrefs.GetString("maxEndlosweiteConfirmation", "");
+        
+        SetupBestenliste();
 
-        ownScore.text = "Ihr Ergebnis:\n" +
-            score + " (#" + GetPlace() + ")";
+        StartCoroutine(GetRank());
     }
 
     public void Submit()
@@ -29,9 +36,35 @@ public class Bestenliste : MonoBehaviour
         print("Submitting score: " + score + " (" + nameInput.text + ")");
     }
 
-    private int GetPlace()
+    private IEnumerator GetRank()
     {
         // TODO: Implementation of Rank
-        return Math.Max(score * -1 + 2000, 1);
+        var myWr = UnityWebRequest.Get(serverAdress + "/" + rankAdress + score);
+        //myWr.SetRequestHeader();
+        yield return myWr.Send();
+        
+        if(myWr.isError) {
+            Debug.LogError(myWr.error);
+        }
+        else {
+            ownScore.text = "Ihr Ergebnis:\n" +
+                            score + " (#" + myWr.downloadHandler.text + ")";
+            
+        }
+    }
+
+    private void SetupBestenliste()
+    {
+        bestenliste.text = "<b>Die Besten:</b>\n" +
+                           "1. Karl (51683)\n" +
+                           "2. Karl (31555)\n" +
+                           "3. Karl (3013)\n" +
+                           "4. Karl (687)\n" +
+                           "5. Karl (311)\n";
+    }
+
+    public void OpenBestenliste()
+    {
+        Application.OpenURL(serverAdress + "/bestenliste.php");
     }
 }
